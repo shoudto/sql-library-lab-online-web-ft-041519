137fa7aba273a292416188fe5b23e551ac172387
Oscar Shoudt <shoudto@gmail.com>
2019-05-26 02:25:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,6 @@ def select_character_names_and_number_of_books_they_are_in
  FROM characters
  INNER JOIN character_books
  ON characters.id = character_books.book_id
- GROUP BY characters.name;"
+ GROUP BY characters.name
+ ORDER BY COUNT(character_books.book_id) DESC;"
 end
