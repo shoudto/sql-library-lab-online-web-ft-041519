7ddc9d9103b0a972ecc105110e8077e25d721123
Oscar Shoudt <shoudto@gmail.com>
2019-05-26 02:17:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ end
 
 def select_series_title_with_most_human_characters
 "SELECT title
- FROM series LIMIT 1"
+ FROM series LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
